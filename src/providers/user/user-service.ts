a2972d0479f8d2cdb91b420714eedc1bef6caa00
Derek Edwards <dedwards8@gmail.com>
2017-07-27 16:35:44 -0400

    Return Real Profile and Display the Name

--- src/providers/user/user-service.ts
+++ src/providers/user/user-service.ts
@@ -24,28 +24,22 @@ export class UserServiceProvider {
 
 
   //This method is not the ideal user option, I believe any changes made to the user this way will not propogate across pages.
-  getUser(){
-//    console.log(this.oneClickProvider.getTestProfile());
+  getUser(): User{
 
-
-    this.oneClickProvider.getTestProfile().then(ocp => console.log(ocp));
-  
     if(this.user == null)
     {
       this.user = new User;
-      this.user.firstName = 'TEST_FIRST_NAME';
-      this.user.lastName = 'TEST_last_NAME';
+      this.user.first_name = 'TEST_FIRST_NAME';
+      this.user.last_name = 'TEST_last_NAME';
       this.user.email = "fakeEmail@example.com"
-      this.user.userId = 123;
-      this.user.location = 'TEST_locATION';
     }
 
     return this.user;
   }
 
-  setUserLocation(location : string){
-    this.user.location = location;
-  }
+  //setUserLocation(location : string){
+  //  this.user.location = location;
+ // }
 
 
 
