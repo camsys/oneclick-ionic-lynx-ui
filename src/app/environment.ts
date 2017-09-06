7921c10aec947dd53d04e3598d1b68cc2b714ee6
Nicolas Garcia <ngarcia@camsys.com>
2017-09-06 15:57:39 -0400

    Simplify user-locator page

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/'
+  // BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/'
+  BASE_ONECLICK_URL: 'http://occ-lynx-dev.herokuapp.com/api/v2/'
 };
