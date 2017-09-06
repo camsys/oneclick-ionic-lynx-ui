588334ed0ce552d838ca379f2a2859827321fed5
mmaranda-cs <mmaranda@camsys.com>
2017-09-06 16:10:09 -0400

    Replace refer with one click (#15)
    * This is not the approach I ultimately want to use but it is a step in the right direction. I was hoping to find some tie in directly to google translate APIs but nothing has come up yet.
    
    * Updating the oneclick service to consume refernet data now stored in one-click.
    
    Updated the UI to use the one-click renames for refernet models and attributes on models
    
    Updated Page names to reflect these renames
    
    --TODO update the models to reflect the renames
    --TODO remove the old pages
    
    * update the models to reflect the renames
    remove the old pages
    
    * Removing references to the userProvider. It is no longer necessary.
    
    * Setting this up to deal with time and sort the time if it is available.
    
    * Now runs what we want it to run.
    
    * Now runs what we want it to run.
    
    * Adding the trips model.

---
+++ src/models/trip.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by mmaranda on 6/21/17.
+ */
+
+export class TripModel {
+  origin_attributes: {};
+  destination_attributes: {};
+  arrive_by: boolean;
+  trip_time: string; //iso8601 time string
+  purpose: string;
+}
