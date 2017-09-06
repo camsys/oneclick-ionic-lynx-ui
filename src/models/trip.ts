8f12fce89ba0ec48c0cc9e1682235084228e2d52
mmaranda-cs <mmaranda@camsys.com>
2017-09-06 16:39:56 -0400

    Updating the tripmodel to include locationModel

--- src/models/trip.ts
+++ src/models/trip.ts
@@ -1,10 +1,12 @@
+import { LocationModel } from './location';
+
 /**
  * Created by mmaranda on 6/21/17.
  */
 
 export class TripModel {
-  origin_attributes: {};
-  destination_attributes: {};
+  origin_attributes: LocationModel;
+  destination_attributes: LocationModel;
   arrive_by: boolean;
   trip_time: string; //iso8601 time string
   purpose: string;
