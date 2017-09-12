746d21664e858682f015302bc1efbcbfc95c993a
Nicolas Garcia <ngarcia@camsys.com>
2017-09-12 16:27:17 -0400

    Add default location to envt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {  
-  BASE_ONECLICK_URL: 'http://occ-lynx-prod.herokuapp.com/api/v2/'
+  BASE_ONECLICK_URL: 'http://occ-lynx-prod.herokuapp.com/api/v2/',
+  DEFAULT_LOCATION: {
+    lat: 28.538336, 
+    lng: -81.379234
+  }
 };
