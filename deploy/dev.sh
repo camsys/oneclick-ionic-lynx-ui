338540ae8882db322db7f11da6c0151fd23c54ba
Nicolas Garcia <ngarcia@camsys.com>
2017-09-22 15:15:30 -0400

    Update deploy scripts based on latest Ionic CLI

--- deploy/dev.sh
+++ deploy/dev.sh
@@ -2,7 +2,6 @@
 
 cp ./src/app/environment.ts ./src/app/environment.ts.bak
 cp ./src/environments/environment.dev.ts ./src/app/environment.ts
-ionic build browser
+ionic cordova build browser
 aws s3 sync ./www/ s3://ui-lynx-dev --acl public-read  --region us-east-2 --profile lynx
 mv ./src/app/environment.ts.bak ./src/app/environment.ts
-
--- deploy/prod.sh
+++ deploy/prod.sh
@@ -2,6 +2,6 @@
 
 cp ./src/app/environment.ts ./src/app/environment.ts.bak
 cp ./src/environments/environment.prod.ts ./src/app/environment.ts
-ionic build browser
+ionic cordova build browser
 aws s3 sync ./www/ s3://ui-lynx-prod --acl public-read  --region us-east-2 --profile lynx
 mv ./src/app/environment.ts.bak ./src/app/environment.ts
