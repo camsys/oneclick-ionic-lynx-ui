0fa372e748201ce18dc4d54bca20a644e70d9c60
Derek Edwards <dedwards8@gmail.com>
2017-09-22 17:03:01 -0400

    Revert "Alerts(#22)"
    This reverts commit b76d7df34d67cee7d3bb4b84822f5d72e58b2c83.

--- src/pages/help-me-find/help-me-find.ts
+++ src/pages/help-me-find/help-me-find.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
 import { UserLocatorPage }    from '../user-locator/user-locator';
 // import { TransportationAgenciesPage } from '../transportation-agencies/transportation-agencies';
 import { DirectTransporationFinderPage } from '../direct-transporation-finder/direct-transporation-finder';
-
-// PROVIDERS
-import { OneClickProvider } from '../../providers/one-click/one-click';
-
-// MODELS
-import { Alert } from '../../models/alert';
 
 /**
  * Generated class for the HelpMeFindPage page.
@@ -24,17 +17,11 @@ import { Alert } from '../../models/alert';
 })
 export class HelpMeFindPage {
 
-  alerts: Alert[];
-
   constructor(public navCtrl: NavController, 
-              public navParams: NavParams,
-              private alertCtrl: AlertController,
-              public oneClickProvider: OneClickProvider) {
+              public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {  
-    this.oneClickProvider.getAlerts()
-      .then(alerts => this.alerts = alerts)
+  ionViewDidLoad() {    
   }
 
   openResourcesPage() {
@@ -43,30 +30,6 @@ export class HelpMeFindPage {
 
   openTransportationPage() {
     this.navCtrl.push(UserLocatorPage, { findServicesView: false});
-  }
-
-  presentAlerts() {
-
-    document.getElementById('messages-button').style.display = "none";
-
-    for(let entry of this.alerts) {
-      console.log(entry);
-      let alert = this.alertCtrl.create({
-        title: entry.subject,
-        subTitle: entry.message,
-        buttons: [{
-          text: 'OK',
-          handler: () => {
-            this.ackAlert(entry);
-          }
-        }]
-      });
-      alert.present();
-    }
-  }
-
-  ackAlert(alert: Alert){
-    this.oneClickProvider.ackAlert(alert);
   }
 
 }
