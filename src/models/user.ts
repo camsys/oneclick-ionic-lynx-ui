33318e20f284b1b1053ae180d421fbbc2d5c0938
Derek Edwards <dedwards8@gmail.com>
2017-09-22 09:38:24 -0400

    Add alerts model to User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,6 +6,7 @@ export class User {
   first_name: string;
   last_name: string;
   email: string;
+  alerts: Alert[];
   accommodations: Accommodation[];
   eligibilities: Eligibility[]; 
   preferred_locale: string;
@@ -29,4 +30,10 @@ export class TripType {
   code: string;
   name: string;
   value: boolean;
+}
+
+export class Alert {
+  id: number; 
+  subject: string;
+  message: string;
 }
