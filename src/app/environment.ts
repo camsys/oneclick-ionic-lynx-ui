e21356dc155f58608f5a94a133e5536baf795a36
Derek Edwards <dedwards8@gmail.com>
2017-09-22 10:50:54 -0400

    Update version.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
-export const environment = {
-  BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/',
+export const environment = {  
+  BASE_ONECLICK_URL: 'http://occ-lynx-qa.herokuapp.com/api/v2/',
   DEFAULT_LOCATION: {
     lat: 28.538336, 
     lng: -81.379234
