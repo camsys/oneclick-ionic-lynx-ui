bc2c6be5367ff0b8e7fcbdc7866f725172a2ed11
Nicolas Garcia <ngarcia@camsys.com>
2017-09-25 13:21:42 -0400

    Update Place Search to refresh quickly

--- src/components/place-search/place-search.ts
+++ src/components/place-search/place-search.ts
@@ -45,7 +45,6 @@ export class PlaceSearchComponent {
     this.searchControl.valueChanges
                       .debounceTime(500)
                       .subscribe((query) => {
-      console.log("SUBMITTING QUERY", query);
       this.updateAddressSearch(query);
     });
   }
@@ -62,7 +61,6 @@ export class PlaceSearchComponent {
     .subscribe(places => {
       // Set oneClickAutocompleteItems to the places call results and refresh the search results
       this.oneClickAutocompleteItems = places;
-      console.log("ONECLICK RESULTS RETURNED", this.oneClickAutocompleteItems);
       this.refresh();
     });
 
@@ -71,7 +69,6 @@ export class PlaceSearchComponent {
     .subscribe(places => {
       // Set googleAutocompleteItems to the places call results and refresh the search results
       this.googleAutocompleteItems = places;
-      console.log("GOOGLE RESULTS RETURNED", this.googleAutocompleteItems);
       this.refresh();
     });
     
@@ -81,8 +78,7 @@ export class PlaceSearchComponent {
   private refresh() {
     // Set autocomplete results to the combination of the google and oneclick place searches
     this.autocompleteItems = this.googleAutocompleteItems.concat(this.oneClickAutocompleteItems);
-    this.changeDetector.detectChanges();
-    console.log("SEARCH RESULTS REFRESHED", this.autocompleteItems);
+    this.events.publish('place-search:change');
   }
   
   // Empties the search results array
@@ -98,11 +94,9 @@ export class PlaceSearchComponent {
   
   // Select an item from the search results list
   chooseItem(item: any) {
-    console.log("ITEM CHOSEN!", item);
     this.events.publish('spinner:show'); // Show spinner until geocoding call returns
     this.geoServiceProvider.getPlaceFromFormattedAddress(item)
     .subscribe((places) => {
-      console.log("PLACES RETURNED", places);
       this.setPlace(places[0]);
       this.clear();
       this.events.publish('spinner:hide'); // Hide spinner once places are returned
