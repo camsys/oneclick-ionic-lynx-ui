4c754806de97ba3cfc9d6a9fa0c0564a2c23b2ed
Nicolas Garcia <ngarcia@camsys.com>
2017-10-18 16:20:24 -0400

    Install packages after merging 0.3.0

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {  
-  BASE_ONECLICK_URL: 'http://occ-lynx-dev.herokuapp.com/api/v2/',
-  // BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/',
+  // BASE_ONECLICK_URL: 'http://occ-lynx-dev.herokuapp.com/api/v2/',
+  BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/',
   DEFAULT_LOCATION: {
     lat: 28.538336, 
     lng: -81.379234
