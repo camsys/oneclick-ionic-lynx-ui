aae09b82eeac1019fe64f64983a04d346e60d49c
Nicolas Garcia <ngarcia@camsys.com>
2017-10-25 14:47:41 -0400

    Translate service tabs and prettyTime

--- src/pipes/pretty-time.ts
+++ src/pipes/pretty-time.ts
@@ -1,25 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 /*
  * Transform a number in seconds to a nice, concise time string
 */
 @Pipe({name: 'prettyTime'})
 export class PrettyTimePipe implements PipeTransform {
+  
+  constructor(private translate: TranslateService) { }
+  
   transform(seconds: number | null | undefined): string {
-    if(!seconds) { return "unknown"; } // Return string "unknown" if value is null
+    if(!seconds) { return this.translate.instant("global.time.unknown"); } // Return string "unknown" if value is null
     let timeString = "";
     
     // populate hours
     let hours = Math.floor(seconds / 3600);
     seconds %= 3600;    
     if(hours > 0) {
-      timeString += (hours + " hr ");
+      timeString += (hours + " " + this.translate.instant("global.time.hour_short") + " ");
     }
     
     // populate minutes
     let minutes = Math.floor(seconds / 60);
     seconds %= 60;    
     if(minutes > 0) {
-      timeString += (minutes + " min");
+      timeString += (minutes + " " + this.translate.instant("global.time.minute_short"));
     }
     
     return timeString;
