87f41d47d11fce70a72adad41a11935c118c2252
Nicolas Garcia <ngarcia@camsys.com>
2017-10-31 16:46:30 -0400

    Update Accom & Eligibility translation

--- src/models/accommodation.ts
+++ src/models/accommodation.ts
@@ -2,5 +2,7 @@
 export class Accommodation {
   code: string;
   name: string;
+  note: string;
+  question: string;
   value: boolean;
 }
--- src/models/eligibility.ts
+++ src/models/eligibility.ts
@@ -2,5 +2,7 @@
 export class Eligibility {
   code: string;
   name: string;
+  note: string;
+  question: string;
   value: boolean;
 }
