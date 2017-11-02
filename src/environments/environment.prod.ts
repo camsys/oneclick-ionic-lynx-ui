199935dcf5b5ab39f119a8f2f359b6ec4d46c5ea
Nicolas Garcia <ngarcia@camsys.com>
2017-11-02 14:57:08 -0400

    Update environment files with AWS URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {  
   BASE_ONECLICK_URL: 'http://occ-lynx-prod.herokuapp.com/api/v2/',
-  AWS_BUCKET: 'https://s3.amazonaws.com/occ-lynx-prod/i18n/',
+  AWS_BUCKET: 'https://s3.us-east-2.amazonaws.com/occ-lynx-prod/i18n/',
   DEFAULT_LOCATION: {
     lat: 28.538336, 
     lng: -81.379234
--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {  
   BASE_ONECLICK_URL: 'http://occ-lynx-qa.herokuapp.com/api/v2/',
-  AWS_BUCKET: 'https://s3.amazonaws.com/occ-lynx-qa/i18n/',
+  AWS_BUCKET: 'https://s3.us-east-2.amazonaws.com/occ-lynx-qa/i18n/',
   DEFAULT_LOCATION: {
     lat: 28.538336, 
     lng: -81.379234
