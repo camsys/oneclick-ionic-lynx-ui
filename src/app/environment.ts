bf0007ffb4c3bf7d95d133f5d022f0ab54ee257c
Derek Edwards <dedwards8@gmail.com>
2018-03-29 09:24:44 -0400

    Add helper text.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,10 +1,9 @@
-export const environment = {
-  //BASE_ONECLICK_URL: 'http://occ-lynx-qa.herokuapp.com/api/v2/',
-  BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/',
-  AWS_LOCALE_BUCKET: 'https://s3.us-east-2.amazonaws.com/occ-lynx-dev/i18n/',
-  AWS_IMAGE_ASSET_BUCKET: 'https://s3.us-east-2.amazonaws.com/occ-lynx-dev/assets/img/',
+export const environment = {  
+  BASE_ONECLICK_URL: 'http://occ-lynx-prod.herokuapp.com/api/v2/',
+  AWS_LOCALE_BUCKET: 'https://s3.us-east-1.amazonaws.com/occ-lynx-prod/i18n/',
+  AWS_IMAGE_ASSET_BUCKET: 'https://s3.us-east-1.amazonaws.com/occ-lynx-prod/assets/img/',
   DEFAULT_LOCATION: {
-    lat: 28.538336,
+    lat: 28.538336, 
     lng: -81.379234
   },
   AVAILABLE_LOCALES: [ 'en', 'es', 'pt', 'vi', 'ht', 'keys' ],
