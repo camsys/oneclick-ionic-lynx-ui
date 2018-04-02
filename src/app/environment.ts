59521e3666ca7b8b7bfb92b66fe8039882f236ca
Derek Edwards <dedwards8@gmail.com>
2018-04-02 11:09:43 -0400

    Show helper text on the transportation options page.

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {  
-  BASE_ONECLICK_URL: 'http://localhost:3000/api/v2/',
+  BASE_ONECLICK_URL: 'https://occ-lynx-prod.herokuapp.com/api/v2/',
   AWS_LOCALE_BUCKET: 'https://s3.us-east-1.amazonaws.com/occ-lynx-prod/i18n/',
   AWS_IMAGE_ASSET_BUCKET: 'https://s3.us-east-1.amazonaws.com/occ-lynx-prod/assets/img/',
   DEFAULT_LOCATION: {
