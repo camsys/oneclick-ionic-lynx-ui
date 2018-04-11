2304fc11f5bd35dd74e7c1e99c43a328db980964
Derek Edwards <dedwards8@gmail.com>
2018-04-11 13:57:20 -0400

    Sms (#52)
    * SMS Button to the 211 PAGE
    
    * The SMS Button works.
    
    * Show message on sign out and go to the home page.

---
+++ src/pages/sms-modal/sms-modal.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { ServiceModel } from "../../models/service";
+import { IonicPage, ViewController, NavParams, ToastController } from 'ionic-angular';
+import { OneClickProvider } from '../../providers/one-click/one-click';
+import { FormBuilder, FormGroup, } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+
+@IonicPage()
+@Component({
+  selector: 'page-sms-modal',
+  templateUrl: 'sms-modal.html',
+})
+export class SmsModalPage {
+
+  smsForm: FormGroup;
+  service: ServiceModel;
+  services: ServiceModel[];
+
+  constructor(public navParams: NavParams,
+              public viewCtrl: ViewController,
+              public oneClick: OneClickProvider,
+              private formBuilder: FormBuilder,
+              private toastCtrl: ToastController,
+              private translate: TranslateService) {
+     this.service = navParams.get('service');
+     this.services = navParams.get('services');
+     this.smsForm = this.formBuilder.group({
+      sms: ['']
+    });
+  }
+
+  cancel() {
+    this.viewCtrl.dismiss(null);
+  }
+
+  send(){
+
+    var ids = new Array();
+
+    // Get Ids of Services Array if it was passed
+    if(this.services != null){
+      for (var i = 0; i < this.services.length; i++) {
+        ids.push(this.services[i].id);
+      }
+    }
+
+    // Get ID of a single service if it was passed
+    if(this.service != null){
+      ids.push(this.service.id);
+    }
+
+    this.oneClick.sms211Service(this.smsForm.value['sms'],ids);
+    this.viewCtrl.dismiss(null);
+    let toast = this.toastCtrl.create({
+      message: this.translate.instant('lynx.pages.sms.sms_sent'),
+      position: 'bottom',
+      duration: 3000
+    });
+    toast.present();
+
+  }
+
+  ionViewDidLoad() {
+  }
+
+}
